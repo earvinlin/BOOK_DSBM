dd6b821e0aae98b05889ab53baa885e687cc3044
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2024-06-10 21:57:19 +0800

    20240610-2158 add code.

--- testSymPy_ch03.py
+++ testSymPy_ch03.py
@@ -29,7 +29,27 @@ y_values = [1 for _ in range(sample_count)]
 px.histogram(x=x_values, y=y_values, nbins=20).show()
 """
 
+#-- Sample 3-16 --#
+print("#-- Sample 3-16 --#")
+
+from scipy.stats import norm
+
+def critical_z_value(p) :
+    norm_dist = norm(loc = 0.0, scale = 1.0)
+    left_tail_area = (1.0 - p) / 2.0
+    upper_area = 1.0 - ((1.0 - p) / 2.0)
+    return norm_dist.ppf(left_tail_area), norm_dist.ppf(upper_area)
+
+print(critical_z_value(p = .95))
+# (-1.959963984540054, 1.959963984540054)
+
+
+print("\n\n\n")
+print("==============")
+print("\n")
+#-- Testing Area --#
 import random
+
 x_values = [(sum([random.uniform(0.0, 1.0) for i in range(31)]) / 31) for _ in range(10)]
 #           ---------------------------------------------------------
 print(x_values)
